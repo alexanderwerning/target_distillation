6d9b154e7f5d09a39f7ba42c7cf879a6474c7428
Alexander Werning <werning@nt.upb.de>
2024-04-24 11:14:24 +0200

    Reduce model wrapper to forward output matching

--- target_distillation/model_wrapper.py
+++ target_distillation/model_wrapper.py
@@ -1,27 +1,17 @@
-from pathlib import Path
-
-import numpy as np
 import torch
 
+# thin wrapper to match output parameters
 
 class ModelWrapper(torch.nn.Module):
-    def __init__(self, model, feature_extractor=None, weight_path=None):
+    def __init__(self, model, has_features=True):
         super().__init__()
         self.model = model
-        self.feature_extractor = feature_extractor
-        if weight_path is not None:
-            assert Path(weight_path).exists()
-            self.model.load_state_dict(torch.load(weight_path))
+        self.has_features = has_features
     
-    def forward(self, batch):
-        if self.feature_extractor is not None:
-            assert batch["audio_data"].ndim == 3, batch["audio_data"].shape
-            assert batch["audio_data"].shape[1] == 1, batch["audio_data"].shape
-            float_input = batch["audio_data"].squeeze(1)
-            mel_spec = self.feature_extractor(float_input)[:, None]
+    def forward(self, mel_spec):
+        if self.has_features:
+            x, features = self.model(mel_spec)
         else:
-            assert isinstance(batch, (torch.Tensor, np.ndarray)), type(batch)
-            mel_spec = batch
-
-        x, features = self.model(mel_spec)
+            x = self.model(mel_spec)
+            features = None
         return x, features
