ef3839d30317425315c91c8b8f9c9b15f700a62c
Alexander Werning <werning@nt.upb.de>
2024-09-25 17:32:46 +0200

    Add clap wrapper

--- target_distillation/model_wrapper.py
+++ target_distillation/model_wrapper.py
@@ -1,4 +1,5 @@
 import torch
+from transformers import ClapProcessor, ClapModel
 
 # thin wrapper to match output parameters
 
@@ -15,3 +16,23 @@ class ModelWrapper(torch.nn.Module):
             x = self.model(mel_spec)
             features = None
         return x, features
+
+class ClapModelWrapper(torch.nn.Module):
+    def __init__(self, pretrained_model_name_or_path):
+        super().__init__()
+        self.processor = ClapProcessor.from_pretrained(pretrained_model_name_or_path)
+        self.model = ClapModel.from_pretrained(pretrained_model_name_or_path)
+    
+    def encode_audio(self, audio_data):
+        # cast back to numpy
+        audio_data = [a[0] for a in audio_data.cpu().numpy()]
+        
+        inputs = self.processor(audios=audio_data, return_tensors="pt", sampling_rate=32000)
+        return self.model.get_audio_features(**inputs)
+    
+    def forward(self, inputs):
+        return self.encode_audio(inputs)
+
+class NoFeatureExtractor(torch.nn.Module):
+    def forward(self, inputs):
+        return inputs
